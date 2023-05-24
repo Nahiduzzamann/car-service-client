8a72f0c3607519c90124f394a34f585de193fce8
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-05-24 18:37:52 +0600

    set environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA96lTUU7Qr153AoyBjc5xZBwyBl0NUorc",
-  authDomain: "car-service-1e89b.firebaseapp.com",
-  projectId: "car-service-1e89b",
-  storageBucket: "car-service-1e89b.appspot.com",
-  messagingSenderId: "841384933886",
-  appId: "1:841384933886:web:903b5ad20c5f7bd2e728af"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
